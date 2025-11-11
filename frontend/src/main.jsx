bcbb17011d2f11f47e6b83ad97e5f69928a53374
Harshit-goswami2506 <harshitashokkumar1234@gmail.com>
2025-11-11 09:42:24 +0530

    Change server URL to production endpoint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-export const serverURL="http://localhost:8000"
+export const serverURL="https://smart-donate-backend.onrender.com"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
